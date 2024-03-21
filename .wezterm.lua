b1d30c8029481ae705dddd78adc342fb3fd296bc
Bruno Santos <hsbruno1@gmail.com>
2024-03-21 21:51:39 +0000

    new

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,6 +3,9 @@ local wezterm = require("wezterm")
 -- local mux = wezterm.mux
 -- This table will hold the configuration.
 local config = {}
+local colors = require("lua/rose-pine").colors()
+local window_frame = require("lua/rose-pine").window_frame()
+local act = wezterm.action
 
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
@@ -13,28 +16,39 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Rosé Pine (Gogh)"
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 16
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+-- config.font = wezterm.font("FiraCode Nerd Font")
+config.font_size = 14
 config.window_padding = {
 	bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
 }
-config.integrated_title_button_style = "MacOsNative"
 config.use_fancy_tab_bar = false
-config.initial_cols = 220
-config.initial_rows = 50
+config.initial_cols = 200
+config.initial_rows = 45
 
 config.colors = {
 	tab_bar = {
 		-- background from poimandres
 		-- background = "#1b1e28",
 		-- background for catppuccin
-		background = "#1E1E2E",
+		-- background = "#1E1E2E",
+		-- background for Kanagawa
+		-- background = "#1F1F28",
+		-- background for rose-pine
+		-- background = "#191724",
 	},
 }
 
--- start in fullscreen
+--
+config.window_frame = window_frame
+config.colors = colors
+config.default_prog = { "C:\\Program Files\\Git\\bin\\bash.exe" }
+
+-- start in fullscreen--
 -- wezterm.on("gui-startup", function(cmd)
 -- 	local tab, pane, window = mux.spawn_window(cmd or {})
 -- 	window:gui_window():maximize()
