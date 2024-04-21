02e76624e0a3a61863bd9c7b1f900b916204c083
Bruno Santos <hsbruno1@gmail.com>
2024-04-21 01:39:14 +0100

    add new gruvbox yep

--- kickstart.nvim/lua/plugins/colorscheme.lua
+++ kickstart.nvim/lua/plugins/colorscheme.lua
@@ -1,27 +1,27 @@
-return {
-  {
-    name = 'rose-pine',
-    'rose-pine/neovim',
-    priority = 1000,
-    opts = {
-      dim_inactive_windows = false,
-      styles = {
-        bold = true,
-        italic = false,
-        transparency = true,
-      },
-      highlight_groups = {
-        StatusLine = { fg = 'iris', bg = 'iris', blend = 10 },
-        CursorLineNr = { fg = 'love' },
-        -- StatusLineNC = { fg = 'subtle', bg = 'surface' },
-      },
-    },
-    init = function()
-      vim.cmd.colorscheme 'rose-pine'
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
-}
+-- return {
+--   {
+--     name = 'rose-pine',
+--     'rose-pine/neovim',
+--     priority = 1000,
+--     opts = {
+--       dim_inactive_windows = false,
+--       styles = {
+--         bold = true,
+--         italic = false,
+--         transparency = true,
+--       },
+--       highlight_groups = {
+--         StatusLine = { fg = 'iris', bg = 'iris', blend = 10 },
+--         CursorLineNr = { fg = 'love' },
+--         -- StatusLineNC = { fg = 'subtle', bg = 'surface' },
+--       },
+--     },
+--     init = function()
+--       vim.cmd.colorscheme 'rose-pine'
+--       vim.cmd.hi 'Comment gui=none'
+--     end,
+--   },
+-- }
 
 -- return {
 --   {
@@ -161,3 +161,35 @@ return {
 --     end,
 --   },
 -- }
+
+return {
+  {
+    'sainnhe/gruvbox-material',
+    enabled = true,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_transparent_background = 0
+      vim.g.gruvbox_material_disable_italic_comment = 1
+      vim.g.gruvbox_material_foreground = 'material' -- material, mix, or original
+      vim.g.gruvbox_material_background = 'hard' -- soft, medium, hard
+      vim.g.gruvbox_material_ui_contrast = 'high' -- The contrast of line numbers, indent lines, etc.
+      vim.g.gruvbox_material_float_style = 'dim' -- Background of floating windows
+      vim.g.gruvbox_material_statusline_style = 'material'
+      vim.g.gruvbox_material_better_performance = 1
+
+      vim.api.nvim_create_autocmd('ColorScheme', {
+        group = vim.api.nvim_create_augroup('custom_highlights_gruvboxmaterial', {}),
+        pattern = 'gruvbox-material',
+        callback = function()
+          local config = vim.fn['gruvbox_material#get_configuration']()
+          local palette = vim.fn['gruvbox_material#get_palette'](config.background, config.foreground, config.colors_override)
+          local set_hl = vim.fn['gruvbox_material#highlight']
+          set_hl('CursorLineNr', palette.red, palette.none)
+        end,
+      })
+
+      vim.cmd.colorscheme 'gruvbox-material'
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+}
