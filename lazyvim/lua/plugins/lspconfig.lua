f5e21f3f43fa7bf47c060a6058d08e3f10124b6c
Bruno Santos <bruno.santos@sitewerk.ch>
2024-08-11 17:04:36 +0100

    update lazyvim

--- lazyvim/lua/plugins/lspconfig.lua
+++ lazyvim/lua/plugins/lspconfig.lua
@@ -1,8 +1,42 @@
+vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]])
+vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]])
+
+local border = {
+  { "┌", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "┐", "FloatBorder" },
+  { "│", "FloatBorder" },
+  { "┘", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "└", "FloatBorder" },
+  { "│", "FloatBorder" },
+}
+
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or border
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      servers = { eslint = {} },
+      -- https://www.lazyvim.org/configuration/recipes#add-eslint-and-use-it-for-formatting
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
     },
   },
 }
