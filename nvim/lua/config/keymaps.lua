504335479e5a85b2ee2510658558f7712a466927
Bruno Santos <bruno.santos@sitewerk.ch>
2025-04-09 17:33:26 +0100

    updates

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -103,8 +103,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- move up/down the file while keeping cursor in the middle
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-e>", "<C-u>zz")
-vim.keymap.set("v", "<C-d>", "<C-d>zz")
-vim.keymap.set("v", "<C-e>", "<C-u>zz")
 
 -- Highlight when yanking text
 vim.api.nvim_create_autocmd("TextYankPost", {
